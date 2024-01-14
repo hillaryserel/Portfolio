38ffc12ef3904888f2806c9f2046745db21e1087
Hillary Serel <Serelhillary00@gmail.com>
2024-01-14 23:14:38 +0300

    commit

--- script.js
+++ script.js
@@ -34,6 +34,17 @@ $(document).ready(function(){
     });
 
     // typing text animation script
+    var icon=document.getElementById("icon");
+    icon.onclick = function(){
+        document.body.classList.toggle("dark-theme");
+        if(document.body.classList.contains("dark-theme")){
+            icon.src = "images\sun.png";
+        }else{
+            icon.src="images\moon.png";
+        }
+    }
+
+
     var typed = new Typed(".typing", {
         strings: ["Front end Developer"],
         typeSpeed: 150,
